2536ea07f50545011587faac7c3c3bb61bbb4b8b
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2023-01-22 19:06:55 -0500

    Correct Factory.sol file

--- SolidityFiles/Factory.sol
+++ SolidityFiles/Factory.sol
@@ -1,32 +1,20 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0
 
-import './CloneFactory.sol';
+pragma solidity >=0.7.0 <0.9.0;
+
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./CloneFactory.sol";
 
-contract Factory is CloneFactory {
-     Child[] public children;
-     address parent;
 
-    constructor(address _parent) {
-        parent = _parent;
-     }
+// Child, please remember a message upon initializing X
+// Child, please remember any message updates
+// Child, please go ahead and keep a tally of how many times you've updated the message
+// Child, please be indexable!
 
-     event ChildCreated(uint date, string message, address childAddress);
 
-     function createChild(string memory message) public {
-        Child child = Child(createClone(parent));
-        child.initialize(message);
-        children.push(child);
-        emit ChildCreated(block.timestamp, message, address(child));
-     }
-
-     function getChildren() external view returns(Child[] memory){
-         return children;
-     }
-}
 
 contract Child is Initializable {
+
     string public message = 'Default message';
     uint256 updatedMessageTally = 0;
 
@@ -40,6 +28,33 @@ contract Child is Initializable {
         message = _message;
         updatedMessageTally++;
         emit MessageUpdated(_message, updatedMessageTally);
+    }
+}
+
+contract Factory is CloneFactory {
+
+// Factory, please keep an array of all children you've ever made
+// Factory, can get get that array of those children
+// Factory, can you import the address of the implementation contract you'd like to clone
+// Factory, can you clone a child, can you emit an event when you've created a child?
+
+    Child[] public children;
+    address implementation;
+
+    event ChildCreated(uint date, string, address childAddress);
+
+    constructor(address _implementation) {
+        implementation = _implementation;
+    }
+
+    function getChildren() external view returns(Child[] memory) {
+        return children;
     } 
-    
-  }
+
+    function createChild(string memory message) public {
+        Child child = Child(createClone(implementation));
+        child.initialize(message);
+        children.push(child);
+        emit ChildCreated(block.timestamp, message, address(child));
+    }
+}
