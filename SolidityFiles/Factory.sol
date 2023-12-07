7d527c3f124a9531d0b509920967de6a9d0415b0
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2023-12-07 09:12:16 -0500

    Update Factory.sol

--- SolidityFiles/Factory.sol
+++ SolidityFiles/Factory.sol
@@ -5,14 +5,6 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "./CloneFactory.sol";
 
-
-// Child, please remember a message upon initializing X
-// Child, please remember any message updates
-// Child, please go ahead and keep a tally of how many times you've updated the message
-// Child, please be indexable!
-
-
-
 contract Child is Initializable {
 
     string public message = 'Default message';
@@ -32,11 +24,6 @@ contract Child is Initializable {
 }
 
 contract Factory is CloneFactory {
-
-// Factory, please keep an array of all children you've ever made
-// Factory, can get get that array of those children
-// Factory, can you import the address of the implementation contract you'd like to clone
-// Factory, can you clone a child, can you emit an event when you've created a child?
 
     Child[] private children;
     address implementation;
